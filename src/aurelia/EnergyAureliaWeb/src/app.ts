e3338ad8a3116234e2afaa3cb85141bb6faf396b
Kevin Logan <logk34@hotmail.com>
2016-10-29 21:44:35 -0500

    energy feature started - almost running

--- src/aurelia/EnergyAureliaWeb/src/app.ts
+++ src/aurelia/EnergyAureliaWeb/src/app.ts
@@ -7,6 +7,7 @@ export class App {
     config.title = 'Aurelia';
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
+      { route: ['energy'],  name: 'energy',       moduleId: 'energy',       nav: true, title: 'Energy' },
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
       { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
     ]);
---
+++ src/aurelia/EnergyAureliaWeb/src/energy/index.ts
@@ -0,0 +1,4 @@
+//http://aurelia.io/hub.html#/doc/article/aurelia/framework/latest/app-configuration-and-startup/6
+export function configure(config) {
+  config.globalResources(['./energy']);
+}
