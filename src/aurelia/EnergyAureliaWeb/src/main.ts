f0e7839514f9cb515adeef3c55e7657fb6d19eac
Kevin Logan <logk34@hotmail.com>
2016-10-31 16:28:25 -0500

    move out of the features, since I couldn't get htat working

--- src/aurelia/EnergyAureliaWeb/src/main.ts
+++ src/aurelia/EnergyAureliaWeb/src/main.ts
@@ -4,8 +4,9 @@ import {Aurelia} from 'aurelia-framework';
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging();
-    
+    .developmentLogging()
+    .feature('energy');
+
   // Uncomment the line below to enable animation.
   // aurelia.use.plugin('aurelia-animator-css');
 
