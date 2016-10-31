4f2801cd91df480560cf29ba7b1a584268f93de9
Kevin Logan <logk34@hotmail.com>
2016-10-31 15:09:02 -0500

    folder didn't work, remove examples and use base folder

--- src/aurelia/EnergyAureliaWeb/src/app.ts
+++ src/aurelia/EnergyAureliaWeb/src/app.ts
@@ -1,15 +1,12 @@
-import {Router, RouterConfiguration} from 'aurelia-router';
+import { Router, RouterConfiguration } from 'aurelia-router';
 
 export class App {
   public router: Router;
 
   public configureRouter(config: RouterConfiguration, router: Router) {
-    config.title = 'Aurelia';
+    config.title = 'Aurelia Energy';
     config.map([
-      { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
-      { route: ['energy'],  name: 'energy',       moduleId: 'energy',       nav: true, title: 'Energy' },
-      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: ['', 'energy'], name: 'energy', moduleId: 'energy', nav: true, title: 'Energy' }
     ]);
 
     this.router = router;
--- src/aurelia/EnergyAureliaWeb/src/energy/index.ts
+++
@@ -1,4 +0,0 @@
-//http://aurelia.io/hub.html#/doc/article/aurelia/framework/latest/app-configuration-and-startup/6
-export function configure(config) {
-  config.globalResources(['./energy']);
-}
