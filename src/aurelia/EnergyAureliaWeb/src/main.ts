2b190decc41889c427f551c7c76f6f5fc120bc68
Kevin Logan <logk34@hotmail.com>
2016-10-31 16:32:45 -0500

    Revert "move out of the features, since I couldn't get htat working"
    This reverts commit 1f517fbf521d310a2333d11bc7d165a8fb80e5e0.

--- src/aurelia/EnergyAureliaWeb/src/main.ts
+++ src/aurelia/EnergyAureliaWeb/src/main.ts
@@ -4,9 +4,8 @@ import {Aurelia} from 'aurelia-framework';
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging()
-    .feature('energy');
-
+    .developmentLogging();
+    
   // Uncomment the line below to enable animation.
   // aurelia.use.plugin('aurelia-animator-css');
 
