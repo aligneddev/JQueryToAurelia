1d792fcdc17e1c8c90619612958affd034b77e86
Kevin Logan <logk34@hotmail.com>
2016-10-31 21:38:31 -0500

    update versions

--- src/aurelia/EnergyAureliaWeb/src/main.ts
+++ src/aurelia/EnergyAureliaWeb/src/main.ts
@@ -5,7 +5,7 @@ export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
     .developmentLogging();
-  
+
   // Uncomment the line below to enable animation.
   aurelia.use.plugin('aurelia-animator-css');
 
