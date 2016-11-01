b031541c9c8b98527dfa20f86d48cd8f6ccaaeec
Kevin Logan <logk34@hotmail.com>
2016-10-31 21:34:31 -0500

    order by folder, thanks to PR from Ashley Grant

--- src/aurelia/EnergyAureliaWeb/src/app.ts
+++ src/aurelia/EnergyAureliaWeb/src/app.ts
@@ -6,7 +6,8 @@ export class App {
   public configureRouter(config: RouterConfiguration, router: Router) {
     config.title = 'Aurelia Energy';
     config.map([
-      { route: ['', 'energy'], name: 'energy', moduleId: 'energy', nav: true, title: 'Energy' }
+      // http://stackoverflow.com/questions/40348535/organize-by-folder-in-aurelia
+      { route: ['', 'energy'], name: 'energy', moduleId: 'energy/energy', nav: true, title: 'Energy' }
     ]);
 
     this.router = router;
