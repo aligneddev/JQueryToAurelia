92eeb0e15ce8261d34b914743170fb7011f3c1fc
Kevin Logan <logk34@hotmail.com>
2016-11-02 15:45:23 -0500

    click the row, changes the view  - but it's blank

--- src/aurelia/EnergyAureliaWeb/src/app.ts
+++ src/aurelia/EnergyAureliaWeb/src/app.ts
@@ -7,7 +7,9 @@ export class App {
     config.title = 'Aurelia Energy';
     config.map([
       // http://stackoverflow.com/questions/40348535/organize-by-folder-in-aurelia
-      { route: ['', 'energy'], name: 'energy', moduleId: 'energy/energy', nav: true, title: 'Energy' }
+      { route: ['', 'energy'], name: 'energy', moduleId: 'energy/energy', nav: true, title: 'Energy' },
+      { route: 'energy-details', name: 'energy-details',
+        moduleId: 'energy/energyDetailsView', nav: false, title: 'Energy Details' }
     ]);
 
     this.router = router;
